faead61415b6635c66c9c5bc6db31b4438b67f4c
Arion Mathias <arion.mathias@gmail.com>
2023-09-07 16:16:14 -0300

    TEST REFACTOR: using @BeforeEach to setUp class variables insted of local variables

--- src/test/java/com/codex/aposta/service/ApostadorServiceTest.java
+++ src/test/java/com/codex/aposta/service/ApostadorServiceTest.java
@@ -1,24 +1,31 @@
 package com.codex.aposta.service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.codex.aposta.model.Apostador;
 import com.codex.aposta.model.dto.ApostadorIn;
 
 @SpringBootTest
 public class ApostadorServiceTest {
 
 	@Autowired
-	ApostadorService apostadorService;
+	private ApostadorService apostadorService;
+
+	private ApostadorIn apostador;
+
+	@BeforeEach
+	void SetUp() {
+		apostador = new ApostadorIn("Bob", "bob@email.com");
+	}
 
 	@Test
 	public void salvaApostador_true_if_new_id() {
-		ApostadorIn apostador = new ApostadorIn("Bob", "bob@email.com");
-		Apostador idEntity = this.apostadorService.salvaApostador(apostador);
-		
-		Assertions.assertNotNull(idEntity.getId(), "The saved object must be with id not null!");
+		Assertions.assertNotNull(this.apostadorService.salvaApostador(apostador).getId(),
+				"The saved object must be with id not null!");
 	}
+	
+
 }
