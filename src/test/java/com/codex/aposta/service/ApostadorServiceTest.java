8d948e843528c4a7e1e3fe5876a6439fd2d1b42b
Arion Mathias <arion.mathias@gmail.com>
2023-09-11 19:35:09 -0300

    refactor: removing spring context, mocking apostadorService dependencies

--- src/test/java/com/codex/aposta/service/ApostadorServiceTest.java
+++ src/test/java/com/codex/aposta/service/ApostadorServiceTest.java
@@ -1,29 +1,53 @@
 package com.codex.aposta.service;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
+import com.codex.aposta.model.Apostador;
 import com.codex.aposta.model.dto.ApostadorIn;
+import com.codex.aposta.repository.ApostadorRepository;
 
-@SpringBootTest
+//@SpringBootTest //Unit tests doesn't need this
+@ExtendWith(MockitoExtension.class)
 public class ApostadorServiceTest {
 
-	@Autowired
+	//@Autowired //Unit tests doesn't need this
+	@InjectMocks
 	private ApostadorService apostadorService;
+	
+	@Mock
+	private ApostadorRepository apostadorMockRepository;
+	
+	@Mock
+	private ModelMapper modelMapperMock;	
 
-	private ApostadorIn apostador;
+	private ApostadorIn apostadorIn;
 
 	@BeforeEach
 	void SetUp() {
-		apostador = new ApostadorIn("Bob", "bob@email.com");
+		apostadorIn = new ApostadorIn("Bob", "bob@email.com");
 	}
 
 	@Test
 	public void salvaApostador_true_if_new_id() {
-		Assertions.assertNotNull(this.apostadorService.salvaApostador(apostador).getId(),
+		
+		when(modelMapperMock.map(this.apostadorIn, Apostador.class))
+		.thenReturn( new Apostador(null,this.apostadorIn.getNome(), this.apostadorIn.getEmail()));
+		
+		when(apostadorMockRepository.save( any(Apostador.class) ))
+			.thenReturn( new Apostador(10L,this.apostadorIn.getNome(), this.apostadorIn.getEmail()));
+		
+		
+		Assertions.assertNotNull( this.apostadorService.salvaApostador(this.apostadorIn).getId(),
 				"The saved object must be with id not null!");
 	}
 	
